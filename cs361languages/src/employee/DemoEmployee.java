0e459e84ea2695e463e1accae489d60ce0cbc80d
Nico Middleton <imiddleton2021@gmail.com>
2024-11-12 14:59:16 -0500

    employee class

--- cs361languages/src/employee/DemoEmployee.java
+++ cs361languages/src/employee/DemoEmployee.java
@@ -32,25 +32,25 @@ public class DemoEmployee {
 		System.out.println("emp3");
 		emp3.whoAmI();
 		// TODO To complete
-		// emp3.whoAmI() // prints ... because ...
-		// emp3.getHourlyRate(); // returns syntax error because ...
+		emp3.whoAmI(); // prints "PART TIME EMPLOYEE" because PartTimeEmployee overrides whoAmI().
+		//emp3.getHourlyRate(); // returns syntax error because emp3 is an Employee which cannot see getHourlyRate().
 
 		// TODO To complete
 		// Add the condition that test that emp3 is an instance of part time employee at
 		// this time
-		// if (CONDITION) {
-		// System.out.println("emp3 instance of part time employee"); // printed
-		// } else {
+		if (emp3 instanceof PartTimeEmployee) {
+			System.out.println("emp3 instance of part time employee"); // printed
+		} else {
 		System.out.println("emp3 not instance of part time employee");
-		// }
+		}
 
 		// Downcasting 1
 		// TODO Uncomment and run the code
-		// PartTimeEmployee pEmp3 = (PartTimeEmployee) emp1;
+		//PartTimeEmployee pEmp3 = (PartTimeEmployee) emp1;
 		// TODO To complete
-		// This instruction compiles / does not compile
+		// This instruction compiles.
 		// This instruction returns a ClassCastException at run time
-		// because ...
+		// because emp1 is an instance of Employee NOT PartTimeEmployee
 
 		// Downcasting 2
 		// Requires the use of intanceof to avoid a run time cast exception
